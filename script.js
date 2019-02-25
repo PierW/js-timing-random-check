aa09110b6b6217c199a5b73d2600fcc26462d55b
Pier.W <factory666@live.it>
2019-02-25 17:53:14 +0100

    Completed

--- script.js
+++ script.js
@@ -6,12 +6,32 @@ function getRandom(min, max) {
 function getRandomArray() {
 
   var arr = [];
-  for (var i=0;i<2;i++) {
+  for (var i=0;i<level;i++) {
 
-    arr.push(getRandom(0, 100));
+    arr.push(getRandom(0, 10));
   }
 
   return arr;
+}
+
+function step2() {
+  if (win) {
+    level += 1;
+    var rndNumber = getRandomArray();
+    var alert = alertArray(rndNumber);
+    setTimeout(askRandomNumber, 2 * 1000, rndNumber);
+  }
+  return;
+}
+
+function step3() {
+  if (win  && level === 3) {
+    level += 1;
+    var rndNumber = getRandomArray();
+    var alert = alertArray(rndNumber);
+    setTimeout(askRandomNumber, 2 * 1000, rndNumber);
+  }
+  return;
 }
 
 function alertArray(arr) {
@@ -41,7 +61,7 @@ function isPresent(elem, arr) {
 }
 
 function askRandomNumber(arr) {
-
+  win = false;
   for (var i=0;i<arr.length;i++) {
 
     var elem = prompt("Dimmi un valore");
@@ -52,11 +72,15 @@ function askRandomNumber(arr) {
       return;
     }
   }
-
   alert("Hai vinto");
+  win = true;
+  return win;
 }
 
+var level = 2;
+var win = false;
 var rndNumber = getRandomArray();
 alertArray(rndNumber);
-
 setTimeout(askRandomNumber, 2 * 1000, rndNumber);
+setTimeout(step2, 2 * 1000);
+setTimeout(step3, 5 * 1000);
